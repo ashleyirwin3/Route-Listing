aa7c30c640f915ced06407987f6fddea35fe7405
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-19 14:02:07 -0600

    completed route listing

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-console.log('hey')
+console.log("ndewdlk")
+
+// create angular module
 
 var app = angular.module('highwayApp2', ['ngRoute'])
+// add ngRoute info app
 
-app.config(($routeProvider, $locationProvider) => {
+app.config( function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('')
   $routeProvider
+    .when('/', {
+      controller: 'MainCtrl',
+      templateUrl: '/partials/main.html',
 
+    })
+
+    .when('/66', {
+      controller: '66Ctrl',
+      templateUrl: 'partials/66.html'
+    })
+    .when('/blue', {
+      controller: 'BlueCtrl',
+      templateUrl: 'partials/blue.html'
+    })
+    .otherwise({redirectTo:'/'})
+})
+
+app.controller('66Ctrl', function($scope) {
+  $scope.highwayName = "Route 66"
+  $scope.highwayDescription = "This is a highway"
+  console.log('66')
+})
+
+app.controller('BlueCtrl', function($scope) {
+  $scope.highwayName = "Blue Ridge Parkway"
+  $scope.highwayDescription= "This is another highway"
+  console.log('blue')
 })
